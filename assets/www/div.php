84f818d671ed92bfa8e7beaa4c1569c8c4d0f8c2
kodevi <kodevi38@gmail.com>
2015-11-19 13:53:33 +0200

    ilk

---
+++ assets/www/div.php
@@ -0,0 +1,35 @@
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='etnak.html'" class="btn-link"><img src="img/1.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;">Kullaným Klavuzu</h5>
+</button>
+</div>
+
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='cozumo.html'" class="btn-link"><img src="img/2.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;text-decoration:none">Çözüm Ortaklarýmýz</h5>
+</button>
+</div>
+		
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='etnaa.html'" class="btn-link"><img src="img/3.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;text-decoration:none">Etna Assistance</h5>
+</button>
+</div>	
+
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='http://www.etnagy.com/memnuniyet-anketi'" class="btn-link"><img src="img/4.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;text-decoration:none">Müþteri Görüþleri</h5>
+</button>
+</div>	
+
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='etnah.html'" class="btn-link"><img src="img/5.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;text-decoration:none">Hakkýmýzda</h5>
+</button>
+</div>	
+
+<div class="col-xs-6 col-sm-4 placeholder">		
+<button type="button" onClick="window.location='acilt.html'" class="btn-link"><img src="img/6.png" class="img-responsive" border="0">
+<h5 class="text" style="color:#FFFFFF;text-decoration:none">Acil Telefonlar</h5>
+</button>
+</div>			
